8031c06c0abe7601d0e6062ac9cefa513129f646
Sandeeppappula <sandeeppappula2005@gmail.com>
2026-07-14 17:12:40 +0530

    Create sum.groovy

---
+++ vars/sum.groovy
@@ -0,0 +1,22 @@
+def sum(a,b)
+{
+  echo "the sum of ${a} and ${b} is ${a+b}"
+}
+
+def sub(a,b)
+{
+  echo "the subtraction of ${a} and ${b} is ${a-b}"
+}
+
+def multi(a,b)
+{
+  echo "the multiplication of ${a} and ${b} is ${a*b}"
+}
+
+def div(a,b)
+{
+  echo "the division of ${a} and ${b} is ${a/b}"
+}
+
+
+
