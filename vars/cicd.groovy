b96daf875f9fa512a9997c48926d23e34755405d
Sandeeppappula <sandeeppappula2005@gmail.com>
2026-07-15 17:10:30 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def gitbuild()
 {
   sh "mvn package"
 }
+def gitdeploy(jobname,ipaddress,contextpath)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${contextpath}'
+}
