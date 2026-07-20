f94b27c8e19b3e9ed1ac11f9c2f98c946c0baf98
Sandeeppappula <sandeeppappula2005@gmail.com>
2026-07-20 23:27:27 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,7 +10,7 @@ def gitdeploy(ipaddress,contextpath)
 {
   sh "scp /var/lib/jenkins/workspace/${JOB_NAME}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${contextpath}.war"
 }
-def gittesting(repo,jobname)
+def gittesting(repo)
 {
   git "https://github.com/Sandeeppappula/${repo}.git"
   sh "java -jar ${WORKSPACE}/${JOB_NAME}/testing.jar"
