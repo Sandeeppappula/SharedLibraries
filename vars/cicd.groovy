4c55aff9708378ee33a372bb898f769534a88582
Sandeeppappula <sandeeppappula2005@gmail.com>
2026-07-20 17:30:28 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,16 +6,16 @@ def gitbuild()
 {
   sh "mvn package"
 }
-def gitdeploy(jobname,ipaddress,contextpath)
+def gitdeploy(ipaddress,contextpath)
 {
-  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${contextpath}.war"
+  sh "scp WORKSPACE/${JOB_NAME}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${contextpath}.war"
 }
 def gittesting(repo,jobname)
 {
   git "https://github.com/Sandeeppappula/${repo}.git"
-  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+  sh "java -jar WORKSPACE/${JOB_NAME}/testing.jar"
 }
-def gitdelivary(jobname,ipaddress,contextpath)
+def gitdelivary(ipaddress,contextpath)
 {
-    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${contextpath}.war"
+    sh "scp WORKSPACE/${JOB_NAME}/webapp/target/webapp.war ubuntu@${ipaddress}:/var/lib/tomcat10/webapps/${contextpath}.war"
 }
